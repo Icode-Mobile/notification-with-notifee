59664615961b5a467712e474480ee704f3cd863c
Caio18-cosenza <caiiocosenza2020@gmail.com>
2023-04-05 11:07:59 -0300

    layout

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Linking from 'expo-linking';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <Text> Notificação com Notifee </Text>
+      <Button title='Enviar notificação' onPress={() => {}} />
     </View>
   );
 }
